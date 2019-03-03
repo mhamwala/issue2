41e436cbc5f40d0bbb4ff4282d8b691ba6e3729d
Ed <98gzi@SQL.com>
2019-03-03 17:18:24 +0000

    dab

---
+++ CPFITNESS/CpMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPFITNESS
+{
+    public partial class CpMain : Form
+    {
+        public CpMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CPFITNESS/LoginForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CPFITNESS
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"server=localhost;user id=root;database=cpfitness;allowuservariables=True");
+            string query = "Select * from users where username = '" + txtUsername.Text.Trim() + "'and password = '" + txtPassword.Text.Trim() + "'";
+            SqlDataAdapter sds = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sds.Fill(dtbl);
+            if (dtbl.Rows.Count == 1)
+            {
+                CpMain objcpMain = new CpMain();
+                this.Hide();
+                objcpMain.Show();
+            }
+            else
+            {
+                MessageBox.Show("Retry");
+            }
+        }
+    }
+}
